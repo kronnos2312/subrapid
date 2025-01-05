44b0ee559e9af1098cf7de86184d35f3540804ca
kronnos2312 <jjoyaquintero@gmail.com>
2025-01-05 11:55:26 -0500

    se sube maqueta generada por el initializa spring boot con dependencia springweb se le hace modificacion sobre el puerto de lanzamiento 8081

---
+++ src/test/kotlin/com/server/tecnology/subrapid/SubrapidApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.server.tecnology.subrapid
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubrapidApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
